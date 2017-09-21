9c9cafcc6a50051925c120a29079832985335c80
Arun Kutty <akutty@deloitte.com>
2017-09-21 19:49:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,62 +1,71 @@
 import os
+from flask import Flask, render_template, request
+from PIL import Image
+import sys
+import pyocr
+import pyocr.builders
+import re
+import json
 
-from flask import Flask, request, render_template, send_from_directory
-
-__author__ = 'ibininja'
+__author__ = 'K_K_N'
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+def ocr(image_file):
+    tools = pyocr.get_available_tools()
+    if len(tools) == 0:
+        print("No OCR tool found")
+        sys.exit(1)
+    # The tools are returned in the recommended order of usage
+    tool = tools[0]
+    print("Will use tool '%s'" % (tool.get_name()))
+    # Ex: Will use tool 'libtesseract'
+
+    langs = tool.get_available_languages()
+    print("Available languages: %s" % ", ".join(langs))
+    lang = langs[1]
+    print("Will use lang '%s'" % (lang))
+
+    txt = tool.image_to_string(
+        Image.open(image_file),
+        lang=lang,
+        builder=pyocr.builders.TextBuilder()
+    )
+    ektp_no = re.search( r'[?:nik\s*:\s*](\d{1,20})\s*', txt, re.I)
+    #print ektp_no
+    #if ektp_no:
+    #    print "ektp_no.group() : ", ektp_no.group()
+    data = {}
+    data['ektp'] = ektp_no.group().strip()
+    return json.dumps(data)
 
 @app.route("/")
 def index():
     return render_template("upload.html")
 
-
-@app.route("/upload", methods=["POST"])
+@app.route("/upload", methods=['POST'])
 def upload():
-    folder_name = request.form['superhero']
-    '''
-    # this is to verify that folder to upload to exists.
-    if os.path.isdir(os.path.join(APP_ROOT, 'files/{}'.format(folder_name))):
-        print("folder exist")
-    '''
-    target = os.path.join(APP_ROOT, 'files/{}'.format(folder_name))
+    target = os.path.join(APP_ROOT, 'images/')
     print(target)
+
     if not os.path.isdir(target):
         os.mkdir(target)
-    print(request.files.getlist("file"))
-    for upload in request.files.getlist("file"):
-        print(upload)
-        print("{} is the file name".format(upload.filename))
-        filename = upload.filename
-        # This is to verify files are supported
-        ext = os.path.splitext(filename)[1]
-        if (ext == ".jpg") or (ext == ".png"):
-            print("File supported moving on...")
-        else:
-            render_template("Error.html", message="Files uploaded are not supported...")
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
         destination = "/".join([target, filename])
-        print("Accept incoming file:", filename)
-        print("Save it to:", destination)
-        upload.save(destination)
+        print(destination)
+        file.save(destination)
+        #Return JSON
+        #print txt
+        #file.delete(destination)
 
-    # return send_from_directory("images", filename, as_attachment=True)
-    return render_template("complete.html", image_name=filename)
-
-
-@app.route('/upload/<filename>')
-def send_image(filename):
-    return send_from_directory("images", filename)
-
-
-@app.route('/gallery')
-def get_gallery():
-    image_names = os.listdir('./images')
-    print(image_names)
-    return render_template("gallery.html", image_names=image_names)
-
+    return ocr(destination) 
+    #return json.dumps(txt)
 
 if __name__ == "__main__":
     app.run(port=4555, debug=True)
+
